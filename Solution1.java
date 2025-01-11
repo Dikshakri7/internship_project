6304b0918d28b0bb09ab8cf25ccf57a818923908
Dikshakri7 <dkri2502@gmail.com>
2025-01-11 23:04:32 +0530

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,34 @@
+//Problem statement
+//Write a program to display a welcome message on the screen.
+//
+//Welcome Message:
+//
+//Welcome to Java course!
+//Hello
+//Ninja
+//Output format:
+//Print the welcome message only.
+//Note:-
+//You don't need to return anything. Just print the desired output.
+//Constraints:
+//Time Limit: 1-sec
+//Constraints:
+//Time Limit: 1-sec
+//Sample Output 1:
+//Welcome to Java course!
+//Hello
+//Ninja
+//
+//Explanation Of Sample Input 1:
+//Printed the welcome message.
+
+public class Solution1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java course!");
+        System.out.println("Hello");
+        System.out.println("Ninja");
+
+	}
+
+}
